b7bf68c62fed86a40644d0879d227aec47736323
Yuxuan Liu <yvt@deakin.edu.au>
2021-10-01 18:50:26 +1000

    Initial commit

---
+++ src/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Contact.css'
+
+function Contact()
+{
+    return <div>
+        <form action ="">
+        <input type = "text"  class="formStyle" placeholder = "Contact us" />
+        <a href = "#" class = "fa fa-facebook" ></a>
+        <a href = "#" class = "fa fa-twitter" ></a>
+        <a href = "#" class = "fa fa-youtube" ></a>
+        <a href = "#" class = "fa fa-google" ></a>
+        <a href = "#" class = "fa fa-linkedin" ></a>
+
+        </form>
+    </div>
+}
+
+export default Contact
---
+++ src/Subscribe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Subscribe.css'
+
+function Subscribe()
+{
+    return <div>
+        <form action="">
+            <input type = "text" name="email" class="formStyle" placeholder = "Enter your email" required />
+            <a href="#" class = "formButton">Subscribe</a>
+        </form>
+    </div>
+}
+
+export default Subscribe
